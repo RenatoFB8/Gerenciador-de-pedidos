6566d25ba7e00ba1484cc5aea5b7de8a42d87488
wenderson-juvenal <wendersjuv@gmail.com>
2023-04-27 21:20:07 -0300

    definir rotas

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+const index = require("./routes/index")
+const ingredientes = require("./routes/ingredientes")
+const pedidos = require("./routes/pedidos")
+const produtos = require("./routes/produtos")
+
+app.use("/", index)
+app.use("/ingredientes", ingredientes)
+app.use("/pedidos", pedidos)
+app.use("/produtos", produtos)
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rondando na porta ${PORT}`)
+})
