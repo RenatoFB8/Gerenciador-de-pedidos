c86c3c26a3e466213ed5980131b0ac6e36207732
wenderson-juvenal <wendersjuv@gmail.com>
2023-04-29 21:30:20 -0300

    crud na rota ingredientes

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+// importações
 const express = require("express")
-const path = require('path');
+const path = require('path')
 const app = express()
 const PORT = 3000
+
+// pegando os arquivos do diretório routes
 const index = require("./routes/index")
 const ingredientes = require("./routes/ingredientes")
 const pedidos = require("./routes/pedidos")
 const produtos = require("./routes/produtos")
 
+// definindo view engine e a pasta das views
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
+// definindo diretório de arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// definindo as rotas
 app.use("/", index)
 app.use("/ingredientes", ingredientes)
 app.use("/pedidos", pedidos)
--- routes/ingredientes.js
+++ routes/ingredientes.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const router = express.Router()
+const db = require("../firebase.js")
 
 router.get('/', (req, res) => {
-  res.render("ingredientes")
+  db.collection("ingredientes").get()
+    .then(ingredientes => {
+      let lista = []
+      ingredientes.forEach(ingrediente => {
+        lista.push(ingrediente.data())
+      })
+      console.log(lista)
+    res.send(lista)
+    })
 })
 
+router.post("/add", (req, res) => {
+  let nome = "chocolate"
+  let valor = 1000
+  db.collection('ingredientes').doc(nome).set({nome, valor});
+
+  res.end()
+})
+router.put("/update", (req, res) => {
+  let nomeAntigo = "chocolate"
+  let nomeNovo = "maçã"
+  let valorAntigo = 2000
+  let valorNovo = 100000
+  if (nomeNovo==""){
+     nomeNovo = nomeAntigo
+  }
+  if (valorNovo==""){
+    valorNovo = valorAntigo
+  }
+  db.collection("ingredientes").doc(nomeAntigo).get()
+    .then(data => {
+      db.collection("ingredientes").doc(nomeNovo).set({nome:nomeNovo, valor:valorNovo})
+      db.collection("ingredientes").doc(nomeAntigo).delete()
+    })
+  res.end()
+})
+router.delete("/delete", (req, res) => {
+  let nome = "maçã"
+  db.collection("ingredientes").doc(nome).delete()
+  res.end()
+})
 module.exports = router
