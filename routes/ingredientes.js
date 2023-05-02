c93dcc5e0e98c4d7159e3fa8c0a69feb1a28d3b4
FlavioGabrielB <barrosflavio065@gmail.com>
2023-05-02 03:53:41 -0300

    codigo de verificação da variavel valor

--- routes/ingredientes.js
+++ routes/ingredientes.js
@@ -15,10 +15,26 @@ router.get('/', (req, res) => {
 
 router.post("/add", (req, res) => {
   let nome = req.body.nome
-  let valor = Number(req.body.valor)
-  db.collection('ingredientes').doc(nome).set({nome, valor});
+  let valor = req.body.valor
+  //Troca a "," por "."
+  valor = valor.replace(',','.')
+  let Isnumber = isNaN(valor)
+  //retorna false para número reais
+  if(Isnumber == false){
+    valor = Number(valor)
+    valor = valor.toFixed(2)
+    valor = valor.toString()
+    valor = valor.replace('.',',')
+    db.collection('ingredientes').doc(nome).set({nome, valor});
+    res.redirect("/ingredientes")
+  } else{
+    console.log("O valor digitado não é um número")
+    //let aviso = "O valor digitado não é um número"
+    //res.render("ingredientes", aviso)
+    //wenderson adiciona a variavel lá no ejs
+    //pfvrvozinho
 
-  res.redirect("/ingredientes")
+  }
 })
 
 router.post("/edit", (req, res) => {
