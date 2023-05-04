147a63bd05c3e0ff615ae6114914ecc8d0872018
Renato Barbosa <tenat.renato@gmail.com>
2023-05-03 21:32:35 -0300

    Adicionado botão dos ingredientes e sua função

--- public/script.js
+++ public/script.js
@@ -30,3 +30,23 @@ form.addEventListener('submit', (event) => {
   })
   window.location.reload();
 })
+
+// evento no botao adicionar 
+let button = document.querySelector(".ingHeader > button")
+
+
+    button.addEventListener("click", () => {
+        let div = document.querySelector('.addIngredientes')
+        let efect = document.querySelector('.opacidade')
+
+        // let exitButton = document.querySelector(div [skill="${button.textContent}"] .exit-button)
+
+        // exitButton.addEventListener("click", ()=>{
+        //     div.classList.add("div-hide")
+        //     div.classList.remove("div-show")
+        // })
+
+       div.style.display='flex'
+      efect.style.display='block'
+    })
+
