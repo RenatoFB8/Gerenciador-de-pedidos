505619ec53f98a695f7e5e6e70fbc7686bf525aa
wenderson-juvenal <wendersjuv@gmail.com>
2023-05-04 00:46:57 -0300

    /ingredientes css do=e cada ingrediente

--- public/script.js
+++ public/script.js
@@ -1,52 +1,61 @@
 const form = document.querySelector('#add-ingrediente')
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault()
+	event.preventDefault()
 
-  const formData = new FormData(form)
-  const jsonData = {};
+	const formData = new FormData(form)
+	const jsonData = {};
 
-  for (let [key, value] of formData.entries()) {
-    jsonData[key] = value;
-  }
-  fetch('/ingredientes/add', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(jsonData)
-  })
-  .then(res => res.text())
-  .then(data => {
-    try {
-        let obj = JSON.parse(data)
-        alert(obj.aviso)
-    } catch {
-        return
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  })
-  window.location.reload();
+	for (let [key, value] of formData.entries()) {
+		jsonData[key] = value;
+	}
+	fetch('/ingredientes/add', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(jsonData)
+		})
+		.then(res => res.text())
+		.then(data => {
+			try {
+				let obj = JSON.parse(data)
+				alert(obj.aviso)
+			} catch {
+				return
+			}
+		})
+	window.location.reload();
 })
 
 // evento no botao adicionar 
 let button = document.querySelector(".ingHeader > button")
 
 
-    button.addEventListener("click", () => {
-        let div = document.querySelector('.addIngredientes')
-        let efect = document.querySelector('.opacidade')
+button.addEventListener("click", () => {
+	let div = document.querySelector('.addIngredientes')
+	let efect = document.querySelector('.opacidade')
 
-        // let exitButton = document.querySelector(div [skill="${button.textContent}"] .exit-button)
+	efect.addEventListener("click",()=>{
+		div.style.display = 'none'
+		efect.style.display = 'none'
+	})
+	div.style.display = 'flex'
+	efect.style.display = 'block'
+})
 
-        // exitButton.addEventListener("click", ()=>{
-        //     div.classList.add("div-hide")
-        //     div.classList.remove("div-show")
-        // })
+// evento ingrediente edit
 
-       div.style.display='flex'
-      efect.style.display='block'
-    })
+let buttonsEdit = document.querySelectorAll(".ingrediente div button")
+buttonsEdit.forEach(button=>{
+	button.addEventListener("click", ()=>{
+		let editForm = document.querySelector(`button[nome='${button.getAttribute("nome")}'] + form[action="/ingredientes/edit"]`)
+		let deleteForm = document.querySelector(`button[nome='${button.getAttribute("nome")}'] + form[action="/ingredientes/delete"]`)
+		if (button.textContent=="editar"){
+			editForm.style.display = "block"
+		} else {
+			deleteForm.style.display = "block"
+		}
 
+	})
+}) 
