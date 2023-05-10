c0fcf9344526dbc61d030d13b96278a872f9d67a
FlavioGabrielB <barrosflavio065@gmail.com>
2023-05-09 22:49:16 -0300

    criação do banco de produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const db = require("../firebase.js")
+
 
 router.get('/', (req, res) => {
   res.render("produtos")
+})
+
+router.post("/add", (req, res) => {
+  let produto = req.body.produto
+  let lucro = req.body.lucro
+//criar e add
+  db.collection("produtos").add({
+    produto: produto,
+    lucro: lucro
+  })
+  
 })
 
 module.exports = router
